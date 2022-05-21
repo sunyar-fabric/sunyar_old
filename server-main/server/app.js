25a67a9c458c4d4a61df6106fa0af57e237965fd
mmdhossein <65893340+mmdhossein@users.noreply.github.com>
2022-05-21 19:12:46 +0430

    Update app.js

--- server-main/server/app.js
+++ server-main/server/app.js
@@ -1,4 +1,4 @@
-const express = require("express");
+const express = require("express"); 
 const cookieParser = require("cookie-parser");
 const { saveLog, logging } = require("./utility/logging");
 const { errorHandling } = require("./utility/error/errorHandling");
